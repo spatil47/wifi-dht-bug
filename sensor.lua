7ebf5868f1f53bb6d451a0255bbf837589d2e654
Siddharth Patil <sid@sidpatil.com>
2017-06-15 12:39:08 -0400

    Data captured from DHT every 15 seconds.
    Measurements should be taken at fixed intervals between 15 and 60
    seconds. Even 15-second intervals may be more frequent than necessary.

--- sensor.lua
+++ sensor.lua
@@ -2,15 +2,16 @@ pin = 1
 
 tmr.create():
   alarm(
-  5000,
-  tmr.ALARM_AUTO,
-  function()
-    status, temp, humi, temp_dec, humi_dec = dht.read(pin)
-    if status == dht.OK then
-      print("DHT Temperature: ".. ((temp*9/5)+32) .." ; ".."Humidity: "..humi)
-    elseif status == dht.ERROR_CHECKSUM then
-      print( "DHT Checksum error." )
-    elseif status == dht.ERROR_TIMEOUT then
-      print( "DHT timed out." )
+    15000,
+    tmr.ALARM_AUTO,
+    function()
+      status, temp, humi, temp_dec, humi_dec = dht.read(pin)
+      if status == dht.OK then
+        print("DHT Temperature: ".. ((temp*9/5)+32) .." ; ".."Humidity: "..humi)
+      elseif status == dht.ERROR_CHECKSUM then
+        print( "Temperature/humidity measurement failed due to DHT checksum error." )
+      elseif status == dht.ERROR_TIMEOUT then
+        print( "Temperature/humidity measurement failed due to DHT timeout." )
+      end
     end
-  end)
+  )
