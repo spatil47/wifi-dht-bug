6ce2568c615792b172a9d3302ac680b565bcd418
Siddharth Patil <sid@sidpatil.com>
2017-06-16 18:40:06 -0400

    Added MQTT connectivity to program.
    Currently, the program connects to the MQTT broker and configures its
    last will and testament. The goal is to publish the DHT measurements
    via MQTT on a fixed time interval.

--- sensor.lua
+++ sensor.lua
@@ -1,11 +1,51 @@
-pin = 1
+dht_pin = 1
+
+client_id = string.format("node-%x", node.chipid())
+
+m = mqtt.Client(client_id, 120, mqtt_cfg.user, mqtt_cfg.pwd)
+
+m:lwt(
+  "/outbox/"..client_id.."/lwt",
+  "offline",
+  0,
+  0
+)
+
+m:on(
+  "offline",
+  function(client)
+    print ("Disconnected from MQTT broker.")
+  end
+)
+
+m:on(
+  "message",
+  function(client, topic, data)
+    print(topic .. ":" )
+    if data ~= nil then
+      print(data)
+    end
+  end
+)
+
+m:connect(
+  mqtt_cfg.host,
+  mqtt_cfg.port,
+  0,
+  function(client)
+    print("Connected to MQTT broker " .. mqtt_cfg.host .. " on port " .. mqtt_cfg.port .. ".")
+  end,
+  function(client, reason)
+    print("Failed to connect to MQTT broker due to error " .. reason .. ".")
+  end
+)
 
 tmr.create():
   alarm(
     15000,
     tmr.ALARM_AUTO,
     function()
-      status, temp, humi, temp_dec, humi_dec = dht.read(pin)
+      status, temp, humi, temp_dec, humi_dec = dht.read(dht_pin)
       if status == dht.OK then
         print("DHT Temperature: ".. ((temp*9/5)+32) .." ; ".."Humidity: "..humi)
       elseif status == dht.ERROR_CHECKSUM then
