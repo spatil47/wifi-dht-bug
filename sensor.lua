5aa1604d0430b07bc92c41a95286dd0433019621
Siddharth Patil <sid@sidpatil.com>
2017-06-19 16:40:53 -0400

    Transmit deviceInfo to MQTT broker only if sjson.encode successful.

--- sensor.lua
+++ sensor.lua
@@ -46,51 +46,51 @@ m:subscribe(
     if ok then
       print ("Encoded Crouton device info as JSON string.")
       print ("Result: " .. json)
+      c:publish(
+        "/outbox/"..client_id.."/deviceInfo",
+        json,
+        0,
+        0,
+        function(dht_c)
+          print("Transmitted Crouton device info JSON string to MQTT broker.")
+          tmr.create():
+            alarm(
+              dht_cfg.time,
+              tmr.ALARM_AUTO,
+              function()
+                status, temp, humi, temp_dec, humi_dec = dht.read(dht_cfg.pin)
+                if status == dht.OK then
+                  print("Temperature measured ".. ((temp*9/5)+32) .." degrees Fahrenheit. Humidity is "..humi .. "%.")
+                  dht_c:publish(
+                    "/outbox/"..client_id.."/temperature",
+                    "{ \"value\": " .. ((temp*9/5)+32) .. " }",
+                    0,
+                    0,
+                    function()
+                      print("Transmitted temperature or humidity value to MQTT broker.")
+                    end
+                  )
+                  dht_c:publish(
+                    "/outbox/"..client_id.."/humidity",
+                    "{ \"value\": " .. humi .. " }",
+                    0,
+                    0,
+                    function()
+                      print("Transmitted temperature or humidity value to MQTT broker.")
+                    end
+                  )
+                elseif status == dht.ERROR_CHECKSUM then
+                  print( "Temperature/humidity measurement failed due to DHT checksum error." )
+                elseif status == dht.ERROR_TIMEOUT then
+                  print( "Temperature/humidity measurement failed due to DHT timeout." )
+                end
+              end
+            )
+        end
+      )
     else
       print ("Failed to encode Crouton device info as JSON string.")
     end
-    c:publish(
-      "/outbox/"..client_id.."/deviceInfo",
-      json,
-      0,
-      0,
-      function(dht_c)
-        print("Transmitted Crouton device info JSON string to MQTT broker.")
-        tmr.create():
-          alarm(
-            dht_cfg.time,
-            tmr.ALARM_AUTO,
-            function()
-              status, temp, humi, temp_dec, humi_dec = dht.read(dht_cfg.pin)
-              if status == dht.OK then
-                print("Temperature measured ".. ((temp*9/5)+32) .." degrees Fahrenheit. Humidity is "..humi .. "%.")
-                dht_c:publish(
-                  "/outbox/"..client_id.."/temperature",
-                  "{ \"value\": " .. ((temp*9/5)+32) .. " }",
-                  0,
-                  0,
-                  function()
-                    print("Transmitted temperature or humidity value to MQTT broker.")
-                  end
-                )
-                dht_c:publish(
-                  "/outbox/"..client_id.."/humidity",
-                  "{ \"value\": " .. humi .. " }",
-                  0,
-                  0,
-                  function()
-                    print("Transmitted temperature or humidity value to MQTT broker.")
-                  end
-                )
-              elseif status == dht.ERROR_CHECKSUM then
-                print( "Temperature/humidity measurement failed due to DHT checksum error." )
-              elseif status == dht.ERROR_TIMEOUT then
-                print( "Temperature/humidity measurement failed due to DHT timeout." )
-              end
-            end
-          )
-      end
-    )
   end
 )
 
